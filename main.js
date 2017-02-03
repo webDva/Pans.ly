5339e10085b0a478a663f103095f5227b51b6521
jermaine <jaylarcane@gmail.com>
2017-02-03 17:19:33 -0500

    began writing url creation function

--- main.js
+++ main.js
@@ -11,6 +11,27 @@ router.use(function (req, res, next) {
     next(); // go to the next route and don't stop here
 });
 
+/* We'll create, initially, two API functions:
+ * 
+ *      One for creating a shortened URL hash. This is for initial testing and development purposes.
+ *      
+ *      And one for creating a PostgreSQL database entry of a shortened URL.
+ *      
+ * We'll need to create a third API function, a GET request, for actually redirecting to a
+ * shortened URL.
+ */
+
+
+router.route('/shorten/:url')
+        .post(function(req, res) {
+            url = req.params.url;
+    
+            // now to shorten the URL. just create a new string is all, lol
+            
+});
+
+app.use('/api', router); // might as well prefix with '/api'
+
 app.listen(3000, function () {
   console.log('on port 3000');
 });
