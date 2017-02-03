ad7dfc00cacdfe2e14549457621f841ff604c6f2
jermaine <jaylarcane@gmail.com>
2017-02-02 19:23:08 -0500

    this works

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var express = require('express');
+var app = express();
 
+var router = express.Router();
 
+app.use(express.static('public')); // no jade, read the docs
+
+router.use(function (req, res, next) {
+    // logging
+    console.log('something is happening');
+    next(); // go to the next route and don't stop here
+});
+
+app.listen(3000, function () {
+  console.log('on port 3000');
+});
