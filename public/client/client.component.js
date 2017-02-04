f50816dccede3c23a674b9823b3a2ebe8bfe6924
jermaine <jaylarcane@gmail.com>
2017-02-03 21:35:34 -0500

    $ctrl was the answer

--- public/client/client.component.js
+++ public/client/client.component.js
@@ -1,14 +1,15 @@
 angular.module('client')
         .component('client', {
             templateUrl: 'client/client.template.html',
-            controller: function ClientController() {
+            controller: function ClientController($http) {
                 this.who = 'wa';
                 this.userUrl;
 
                 this.shortenUrl = function () {
-                    $http({
+                    $http.post('/api/shorten/?url=' + this.userUrl, {
                         method: 'POST',
-                        url: '/api/shorten/?url=' + this.userUrl
+                        url: '/api/shorten/?url=' + this.userUrl,
+                        data: '' // don't know about this stray thing
                     }).then(function successCallback(response) {
                         console.log(response);
                     }, function errorCallback(response) {
