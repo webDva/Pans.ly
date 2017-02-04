f0ba5eaac8e12cec724e7c30eca9b4f2c4d75bc8
jermaine <jaylarcane@gmail.com>
2017-02-03 22:17:48 -0500

    debugging for displaying results

--- public/client/client.component.js
+++ public/client/client.component.js
@@ -1,9 +1,10 @@
 angular.module('client')
         .component('client', {
             templateUrl: 'client/client.template.html',
-            controller: function ClientController($http) {
+            controller: function ClientController($scope, $http) {
                 this.who = 'wa';
                 this.userUrl;
+                $scope.newUrl;
 
                 this.shortenUrl = function () {
                     $http.post('/api/shorten/?url=' + this.userUrl, {
@@ -11,6 +12,7 @@ angular.module('client')
                         url: '/api/shorten/?url=' + this.userUrl,
                         data: '' // don't know about this stray thing
                     }).then(function successCallback(response) {
+                        $scope.newUrl = response; // for debugging, really
                         console.log(response);
                     }, function errorCallback(response) {
                         console.log(response);
