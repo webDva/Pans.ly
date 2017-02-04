9f835959292e5b2ca06cd5ad2f02765e4f812168
jermaine <jaylarcane@gmail.com>
2017-02-03 21:13:25 -0500

    huh

--- public/client/client.component.js
+++ public/client/client.component.js
@@ -4,5 +4,16 @@ angular.module('client')
             controller: function ClientController() {
                 this.who = 'wa';
                 this.userUrl;
+
+                this.shortenUrl = function () {
+                    $http({
+                        method: 'POST',
+                        url: '/api/shorten/?url=' + this.userUrl
+                    }).then(function successCallback(response) {
+                        console.log(response);
+                    }, function errorCallback(response) {
+                        console.log(response);
+                    });
+                };
             }
         });
