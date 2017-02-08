fbc7842efdb1651b83d13168616059e7a215e5b0
jermaine <jaylarcane@gmail.com>
2017-02-08 12:40:21 -0500

    looks better and is more clear

--- public/client/client.component.js
+++ public/client/client.component.js
@@ -2,26 +2,26 @@ angular.module('client')
         .component('client', {
             templateUrl: 'client/client.template.html',
             controller: function ClientController($http) {
-                this.who = 'wa';
                 this.userUrl;
-                this.httpResponse;
 
                 this.shortenUrl = function () {
                     $http.post('/api/shorten/?url=' + this.userUrl, {
                         method: 'POST'
-                    }).then(function successCallback(response) {
-                        console.log(response);
-                        $http.post('/api/createEntry', {
-                            method: 'POST',
-                            data: "longUrl=" + response.data.longUrl + "&shortUrl=" + response.data.shortUrl
-                        }).then(function successCallback(response) {
-                            console.log(response);
-                        }, function errorCallback(response) {
-                            console.log(response);
-                        });
-                    }, function errorCallback(response) {
-                        console.log(response);
-                    });
+                    })
+                            .then(function successCallback(response) {
+                                console.log(response);
+                                $http.post('/api/createEntry', {
+                                    method: 'POST',
+                                    data: "longUrl=" + response.data.longUrl + "&shortUrl=" + response.data.shortUrl
+                                })
+                                        .then(function successCallback(response) {
+                                            console.log(response);
+                                        }, function errorCallback(response) {
+                                            console.log(response);
+                                        });
+                            }, function errorCallback(response) {
+                                console.log(response);
+                            });
                 };
             }
         });
